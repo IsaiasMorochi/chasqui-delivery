7d39c1edc1650345cc561b6dee6c71508f826a51
IsaiasMorochi <IMorochi@grupobisa.com>
2022-07-17 12:13:36 -0400

    Project Init

---
+++ src/main/java/com/imorochi/chasqui/ChasquiDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.imorochi.chasqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChasquiDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChasquiDeliveryApplication.class, args);
+    }
+
+}
